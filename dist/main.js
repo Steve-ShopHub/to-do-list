43653e67a2db618f332f6b27aa4a3b73f8939be2
Steve <steve@tryshophub.com>
2023-03-08 23:00:37 +0000

    Add further properties to to-do's

--- dist/main.js
+++ dist/main.js
@@ -4,19 +4,32 @@ const createProject = function (title) {
   const project = {
     title,
     array: [],
-    pushToDo: function (toDo) {
-      this.array.push(toDo);
+    addToDo(toDo) {
+      project.array.push(toDo);
+    },
+    deleteToDo(toDo) {
+      project.array.splice(project.array.indexOf(toDo));
     },
   };
-
-  // const pushToDo = (toDo) => project.array.push(toDo);
 
   return project;
 };
 
-const createToDo = function (title) {
+const createToDo = function (
+  title,
+  completed,
+  description,
+  dueDate,
+  priority,
+  checklist
+) {
   const toDo = {
     title,
+    completed: false,
+    description,
+    dueDate,
+    priority,
+    checklist,
   };
   return toDo;
 };
@@ -34,80 +47,10 @@ pushProjectToHolder(priority);
 
 pushProjectToHolder(thisWeek);
 
-thisWeek.pushToDo(homework);
+thisWeek.addToDo(homework);
 
-console.log(thisWeek);
+console.log(thisWeek.array[0]);
 
-// function pushToProject(project, toDo) {
-//   project.array.push(toDo);
-// }
+thisWeek.deleteToDo(homework);
 
-// pushToProject(priority, homework);
-
-// pushToProject(thisWeek, emptyDishwasher);
-
-/*
-
-                **********Chat GPT code *******
-
-const projectHolder = [];
-
-console.log(projectHolder);
-
-const createProject = function (title) {
-  const project = {
-    title,
-    array: [],
-    pushToDo: function (toDo) {
-      this.array.push(toDo);
-    },
-  };
-
-  return project;
-};
-
-const createToDo = function (title) {
-  const toDo = {
-    title,
-  };
-  return toDo;
-};
-
-function pushProjectToHolder(project) {
-  projectHolder.push(project);
-}
-
-const priority = createProject("priority");
-
-const thisWeek = createProject("This Week");
-
-const homework = createToDo("Do Homework");
-
-const emptyDishwasher = createToDo("Empty Dishwasher");
-
-pushProjectToHolder(priority);
-
-console.log(projectHolder);
-
-pushProjectToHolder(thisWeek);
-
-console.log(projectHolder);
-
-console.log(priority);
-console.log(priority.array);
-
-console.log(thisWeek);
-console.log(thisWeek.array);
-
-priority.pushToDo(homework);
-
-console.log(priority);
-console.log(priority.array);
-
-thisWeek.pushToDo(emptyDishwasher);
-
-console.log(thisWeek);
-console.log(thisWeek.array);
-
-
-*/
+console.log(thisWeek.array[0]);
