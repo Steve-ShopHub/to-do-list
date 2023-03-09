428890a4b03ca875033a540e98c6db12fd7612fc
Steve <steve@tryshophub.com>
2023-03-09 16:07:00 +0000

    Use projectHolder to save and load global local storage data

--- dist/temp.js
+++ dist/temp.js
@@ -99,3 +99,93 @@ saveProject(thisWeek);
 nextWeek.addToDo(emptyDishwasher);
 
 saveProject(nextWeek);
+
+////////////////
+
+/*
+
+console.log(localStorage);
+
+(function loadData() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let objDeserialized = JSON.parse(
+      localStorage.getItem(`${localStorage.key(i)}`)
+    );
+    console.log(objDeserialized);
+  }
+})();
+
+function saveProject(project) {
+  let projectSrl = JSON.stringify(project);
+  localStorage.setItem(`${project.title}`, projectSrl);
+}
+
+// function saveToDo(toDo) {
+//   let toDoSrl = JSON.stringify(toDo);
+//   localStorage.setItem(`${toDo.name}`, toDoSrl);
+// }
+
+const projectHolder = [];
+
+const createProject = function (title) {
+  const project = {
+    title,
+    array: [],
+    addToDo(toDo) {
+      project.array.push(toDo);
+      // saveToDo(toDo);
+    },
+    deleteToDo(toDo) {
+      project.array.splice(project.array.indexOf(toDo));
+    },
+  };
+
+  saveProject(project);
+
+  return project;
+};
+
+const createToDo = function (name) {
+  const toDo = {
+    name,
+    title: "",
+    completed: false,
+    description: "",
+    dueDate: "",
+    priority: "",
+    checklist: [],
+  };
+  return toDo;
+};
+
+function pushProjectToHolder(project) {
+  projectHolder.push(project);
+}
+
+/////// Test data ///////////
+
+const nextWeek = createProject("Next Week");
+const thisWeek = createProject("This Week");
+const homework = createToDo("Do Homework");
+
+const dog = createToDo("Walk Dog");
+thisWeek.addToDo(dog);
+
+const washClothes = createToDo("Wash Clothes");
+thisWeek.addToDo(washClothes);
+
+const emptyDishwasher = createToDo("Empty Dishwasher");
+
+pushProjectToHolder(nextWeek);
+
+pushProjectToHolder(thisWeek);
+
+thisWeek.addToDo(homework);
+
+saveProject(thisWeek);
+
+nextWeek.addToDo(emptyDishwasher);
+
+saveProject(nextWeek);
+
+*/
